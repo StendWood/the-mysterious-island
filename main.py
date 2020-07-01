a415efeae48b29188f1b8164c4496073ceaf95d5
StendWood <edwoodzer@gmail.com>
2020-07-01 20:31:59 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+# Imports
+import random
+
+
+# Other code
+import game_data as gdat
+import load_map
+
+# Function
+def adventure():
+    pass
+
+
+if __name__ == "__main__":
+    adventure()
